5d798489e0f4f5dacf1cfb21f69f5910c1eb427c
Mateus Slovak Silva <mateusslovaksilva@gmail.com>
2025-04-23 20:24:30 -0300

    Update Aula04Operadores.java
    nice

--- maratona-java/src/Aula04Operadores.java
+++ maratona-java/src/Aula04Operadores.java
@@ -21,5 +21,41 @@ public class Aula04Operadores {
         System.out.println(DezeIgualQueVinte);
         System.out.println(DezeIgualDez);
         System.out.println(DezeDiferentedeDez);
+
+        // (&& (AND)) (|| (OR)) (! (NOT))
+
+        int idade = 35;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario >= 4612;
+        boolean isDentroDaleiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta "+isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaleiMenorQueTrinta "+isDentroDaleiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 3000;
+        float valorPlaystation = 5000F;
+
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel "+isPlaystationCincoCompravel);
+
+        // = += -= /= %=
+        double bonus = 1800;
+        bonus += + 1000;
+        bonus -= + 1000;
+        bonus *= + 2;
+        bonus /= + 2;
+        bonus %= + 2;
+        System.out.println(bonus);
+
+        //
+        int contador = 0;
+        contador += 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        int contador2 = 0;
+        System.out.println(contador2);
+
     }
 }
