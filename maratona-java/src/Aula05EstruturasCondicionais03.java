54671bf67094da89ddd5fab2e711d0e700b85a9a
Mateus Slovak Silva <mateusslovaksilva@gmail.com>
2025-04-24 20:27:43 -0300

    Create Aula05EstruturasCondicionais03.java
    Estruturas Condicionais

---
+++ maratona-java/src/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,11 @@
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        double salario = 10000;
+        String dismo = "Eu vou dizimar 1000$ ";
+        String doar = "Eu vou doar 1000$ ";
+        String resultado = salario > 5000 ? dismo : doar;
+
+        System.out.println(resultado);
+
+    }
+}
