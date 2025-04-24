5d9bbbd54abbb943310c978f8481b49a9914dc36
Mateus Slovak Silva <mateusslovaksilva@gmail.com>
2025-04-24 20:27:27 -0300

    Create Aula05EstruturasCondicionais01.java
    Estruturas Condicionais

---
+++ maratona-java/src/Aula05EstruturasCondicionais01.java
@@ -0,0 +1,13 @@
+public class Aula05EstruturasCondicionais01 {
+    public static void main(String[] args) {
+        int idade = 16;
+        boolean temIdadeParaBeber = idade >= 18;
+
+        if (temIdadeParaBeber != false) {
+            System.out.println("Tem idade para Beber");
+        }else{
+            System.out.println("nao tem idade para beber");
+        }
+
+    }
+}
