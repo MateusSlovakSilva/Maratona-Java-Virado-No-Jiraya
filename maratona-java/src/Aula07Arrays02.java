bca9e353f7ea5be986a9825261a2226a5b5bab0c
Mateus Slovak Silva <mateusslovaksilva@gmail.com>
2025-04-29 20:26:23 -0300

    Update Aula07Arrays02.java

--- maratona-java/src/Aula07Arrays02.java
+++ maratona-java/src/Aula07Arrays02.java
@@ -1,5 +1,13 @@
 public class Aula07Arrays02 {
     public static void main(String[] args) {
-        
+        String [] nomes = new String[4];
+        nomes[0] = "Goku";
+        nomes[1] = "Luffy";
+        nomes[2] = "Naruto";
+        nomes[3] = "Zoro";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
     }
 }
