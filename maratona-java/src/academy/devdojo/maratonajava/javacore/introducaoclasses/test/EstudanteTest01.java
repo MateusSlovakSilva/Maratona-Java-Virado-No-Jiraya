e7022eb29a1ae8c7d8415b2a6a1086b69ad06a2a
Mateus Slovak Silva <mateusslovaksilva@gmail.com>
2025-05-19 21:10:10 -0300

    n
    n

--- maratona-java/src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -11,6 +11,6 @@ public class EstudanteTest01 {
 
         System.out.println(estudante.nome);
         System.out.println(estudante.idade);
-        
+        System.out.println(estudante.sexo);
     }
 }
