9cc080d9281db795a064e7323d6074721a2cd34f
Mateus Slovak <mateusslovaksilva@gmail.com>
2025-05-27 21:37:55 -0300

    nice

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaoclasses/dominio/Calculadora.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaoclasses/dominio/Calculadora.java
@@ -14,4 +14,25 @@ public class Calculadora {
         System.out.println(num01 * num02);
     }
 
+    public double divideDoisNumeros(double num1, double num2){
+        if (num2 == 0) {
+            return 0;
+        }
+        return num1/num2;
+    }
+
+    public double divideDoisNumeros02(double num1, double num2){
+        if (num2 != 0) {
+            return num1/num2;
+        }
+        return 0;
+    }
+        public void imprimeDivisaoDeDoisNumeros(double num1, double num2){
+            if (num2 == 0) {
+                System.out.println("Nãoe xiste divisão por zero");
+                return;
+            }else{
+            System.out.println(num1/num2);
+            }
+    }
 }
