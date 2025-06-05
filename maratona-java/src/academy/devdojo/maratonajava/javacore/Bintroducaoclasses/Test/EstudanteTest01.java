6a58cd3200882fd1dabb76eba20bf3bda305d52e
MateusSlovakSilva <mateusslovaksilva@gmail.com>
2025-06-05 15:12:23 -0300

    nice

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaoclasses/Test/EstudanteTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaoclasses/Test/EstudanteTest01.java
@@ -1,11 +1,13 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaoclasses.Test;
 
 import academy.devdojo.maratonajava.javacore.Bintroducaoclasses.dominio.Estudante;
+import academy.devdojo.maratonajava.javacore.Bintroducaoclasses.dominio.ImpressoraEstudante;
 
 public class EstudanteTest01 {
     public static void main(String[] args) {
         Estudante estudante01 = new Estudante();
         Estudante estudante02 = new Estudante();
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
 
         estudante01.nome = "Elisa";
         estudante01.idade = 1;
@@ -14,6 +16,9 @@ public class EstudanteTest01 {
         estudante02.nome = "Mateus" ;
         estudante02.idade = 18;
         estudante02.sexo = 'M';
+
+        impressora.imprime(estudante01);
+        impressora.imprime(estudante02);
 
         System.out.println("nome: "+ estudante01.nome);
         System.out.println("Idade: "+ estudante01.idade);
