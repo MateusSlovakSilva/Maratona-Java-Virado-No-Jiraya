f4db811e8d09c3033e6ec39e54804a8c87ca0e7d
MateusSlovakSilva <mateusslovaksilva@gmail.com>
2025-06-10 21:53:28 -0300

    exercicio

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaoclasses/Test/FuncionarioTest01.java
@@ -0,0 +1,4 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaoclasses.Test;
+
+public class FuncionarioTest01 {
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaoclasses/dominio/Funcionarios.java
@@ -0,0 +1,4 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaoclasses.dominio;
+
+public class Funcionarios {
+}
