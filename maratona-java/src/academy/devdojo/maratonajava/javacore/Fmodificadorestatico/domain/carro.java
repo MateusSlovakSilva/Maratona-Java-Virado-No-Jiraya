438a426b625d4922cdaa70b6a350b29ff09d2cfb
MateusSlovakSilva <mateusslovaksilva@gmail.com>
2025-06-23 21:53:24 -0300

    nice

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/carro.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain;
+
+public class carro {
+    private String nome;
+    private double velocidadeMaxima;
+    private double velocidadeLimite = 250;
+}
