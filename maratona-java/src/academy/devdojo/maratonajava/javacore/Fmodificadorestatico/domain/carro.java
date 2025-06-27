92eb1a6f0e612dde94c76d8f6b10056df9dcd77d
MateusSlovakSilva <mateusslovaksilva@gmail.com>
2025-06-26 21:53:11 -0300

    JAVA POO Modificadores Estatico

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/carro.java
+++
@@ -1,7 +0,0 @@
-package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain;
-
-public class carro {
-    private String nome;
-    private double velocidadeMaxima;
-    private double velocidadeLimite = 250;
-}
